547fb416be44277268a800869b123eaf18ec62fe
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-07-12 19:29:46 -0700

    update

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 /* global Vue */
-var app6 = new Vue({
-  el: '#app-6',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
 var app = new Vue({
   el: "#app",
   data: function () {
     return {
-      message: "Hello from JavaScript!",
+      message: "here is a new variable!",
+      name: "briann",
+      showHiddenInfo: false,
+      greyOutInfo: false,
+      fruits: ["raspberries", "mango", "sumo mandarin", "pineapple"],
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
     };
   },
+  methods: {
+    changeMessage: function () {
+      console.log('in change message');
+      this.message = "helloo";
+    },
+    toggle: function () {
+      console.log('in toggle');
+      this.showHiddenInfo = !this.showHiddenInfo;
+    },
+    greyOut: function () {
+      console.log('greying out text')
+      this.greyOutInfo = !this.greyOutInfo;
+    }
+  }
 });
